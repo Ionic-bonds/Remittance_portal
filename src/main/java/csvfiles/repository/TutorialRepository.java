717fb4789221bb21a45f255e4bfe597cce8ed3e9
Yeo Yu Quan <yuquanyeo@gmail.com>
2022-03-08 13:54:10 +0800

    Fixing CSVController, CSVHelper, CSVService

--- src/main/java/csvfiles/repository/TutorialRepository.java
+++ src/main/java/csvfiles/repository/TutorialRepository.java
@@ -1,6 +1,8 @@
 package csvfiles.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import csvfiles.model.Tutorial;
-public interface TutorialRepository extends JpaRepository {
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
 }
--- src/main/java/csvfiles/service/CSVService.java
+++ src/main/java/csvfiles/service/CSVService.java
@@ -1,16 +1,22 @@
 package csvfiles.service;
+
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
 import csvfiles.helper.CSVHelper;
 import csvfiles.model.Tutorial;
 import csvfiles.repository.TutorialRepository;
+
 @Service
 public class CSVService {
   @Autowired
   TutorialRepository repository;
+
   public void save(MultipartFile file) {
     try {
       List<Tutorial> tutorials = CSVHelper.csvToTutorials(file.getInputStream());
@@ -19,6 +25,14 @@ public class CSVService {
       throw new RuntimeException("fail to store csv data: " + e.getMessage());
     }
   }
+
+  public ByteArrayInputStream load() {
+    List<Tutorial> tutorials = repository.findAll();
+
+    ByteArrayInputStream in = CSVHelper.tutorialsToCSV(tutorials);
+    return in;
+  }
+
   public List<Tutorial> getAllTutorials() {
     return repository.findAll();
   }
