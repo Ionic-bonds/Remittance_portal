d285d8e8c79ae30dd59123bfbb3069e2218ceec6
Leonardwkwong <weikit.wong.2019@scis.smu.edu.sg>
2022-03-19 03:43:06 +0800

    Remove UsernameNotFoundException in UserDetailsImpl.java as import statement is not used

--- src/main/java/com/security/services/UserDetailsImpl.java
+++ src/main/java/com/security/services/UserDetailsImpl.java
@@ -8,7 +8,6 @@ import java.util.stream.Collectors;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import com.model.CorporateUser;
 import com.fasterxml.jackson.annotation.JsonIgnore;
