1765b505b5e525a16c7dcd5a6be81b85346b5ffe
David Lim <73053181+davidlyj@users.noreply.github.com>
2022-03-19 18:08:52 +0800

    Update CorporateFieldController.java

--- src/main/java/com/controller/CorporateFieldController.java
+++ src/main/java/com/controller/CorporateFieldController.java
@@ -1,4 +1,5 @@
 package com.controller;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -56,23 +57,27 @@ public class CorporateFieldController {
     // Get Corporate User by corporate_user_id
     @GetMapping("/getCorpUserById/{corporateUserId}")
     public ResponseEntity<CorporateUser> getCorpUserById(@PathVariable("corporateUserId") long corporateUserId) {
-        CorporateUser corporateUser = corporateUserRepository.findById(corporateUserId).orElseThrow(() 
-            -> new ResourceNotFoundException("No Corporate User found with corporate_user_id = " + corporateUserId));
+        CorporateUser corporateUser = corporateUserRepository.findById(corporateUserId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "No Corporate User found with corporate_user_id = " + corporateUserId));
         return new ResponseEntity<>(corporateUser, HttpStatus.OK);
     }
 
     // Add new Corporate User
     @PostMapping("/addCorpUser")
     public ResponseEntity<CorporateUser> addCorpUser(@RequestBody CorporateUser corporateUser) {
-        CorporateUser _corporateUser = corporateUserRepository.save(new CorporateUser(corporateUser.getEmail(), corporateUser.getPassword()));
+        CorporateUser _corporateUser = corporateUserRepository
+                .save(new CorporateUser(corporateUser.getEmail(), corporateUser.getPassword()));
         return new ResponseEntity<>(_corporateUser, HttpStatus.CREATED);
     }
 
     // Update Corporate User
     @PutMapping("/updateCorpUser/{corporateUserId}")
-    public ResponseEntity<CorporateUser> updateCorpUser(@PathVariable("corporateUserId") long corporateUserId, @RequestBody CorporateUser corporateUser) {
+    public ResponseEntity<CorporateUser> updateCorpUser(@PathVariable("corporateUserId") long corporateUserId,
+            @RequestBody CorporateUser corporateUser) {
         CorporateUser _api = corporateUserRepository.findById(corporateUserId)
-            .orElseThrow(() -> new ResourceNotFoundException("No Corporate User found with corporate_user_id = " + corporateUserId));
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "No Corporate User found with corporate_user_id = " + corporateUserId));
         _api.setEmail(corporateUser.getEmail());
         _api.setPassword(corporateUser.getPassword());
         return new ResponseEntity<>(corporateUserRepository.save(_api), HttpStatus.OK);
@@ -87,19 +92,22 @@ public class CorporateFieldController {
         }
         return new ResponseEntity<>(corporateFields, HttpStatus.OK);
     }
+
     // Get Corporate Field by corporate_field_id
     @GetMapping("/getCorpFieldById/{corporateFieldId}")
     public ResponseEntity<CorporateField> getCorpFieldById(@PathVariable("corporateFieldId") long corporateFieldId) {
-        CorporateField corporateField = corporateFieldRepository.findById(corporateFieldId).orElseThrow(() 
-            -> new ResourceNotFoundException("No Corporate Field found with corporate_field_id = " + corporateFieldId));
+        CorporateField corporateField = corporateFieldRepository.findById(corporateFieldId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "No Corporate Field found with corporate_field_id = " + corporateFieldId));
         return new ResponseEntity<>(corporateField, HttpStatus.OK);
     }
 
     // Get all Corporate Fields by corporate_user_id
     @GetMapping("/getAllCorpFieldByCorpUserId/{corporateId}")
-    public ResponseEntity<List<CorporateField>> getAllCorpFieldByCorpUserId(@PathVariable(value = "corporateId") Long corporateId) {
-        CorporateUser searchCorporate = corporateUserRepository.findById(corporateId).orElseThrow(() 
-            -> new ResourceNotFoundException("No Corporate found with corporate_id = " + corporateId));
+    public ResponseEntity<List<CorporateField>> getAllCorpFieldByCorpUserId(
+            @PathVariable(value = "corporateId") Long corporateId) {
+        CorporateUser searchCorporate = corporateUserRepository.findById(corporateId).orElseThrow(
+                () -> new ResourceNotFoundException("No Corporate found with corporate_id = " + corporateId));
         if (searchCorporate == null) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
@@ -112,19 +120,23 @@ public class CorporateFieldController {
 
     // Add new Corporate Field
     @PostMapping("/addCorpField/{corporateUserId}")
-    public ResponseEntity<CorporateField> addCorpField(@PathVariable(value = "corporateUserId") long corporateUserId, @RequestBody CorporateField corporateFieldRequest) {
-        CorporateField corporateField = corporateUserRepository.findById(corporateUserId).map(corporateUser -> { 
+    public ResponseEntity<CorporateField> addCorpField(@PathVariable(value = "corporateUserId") long corporateUserId,
+            @RequestBody CorporateField corporateFieldRequest) {
+        CorporateField corporateField = corporateUserRepository.findById(corporateUserId).map(corporateUser -> {
             corporateFieldRequest.setCorporateUser(corporateUser);
             return corporateFieldRepository.save(corporateFieldRequest);
-        }).orElseThrow(() -> new ResourceNotFoundException("No Corporate User found with corporate_user_id = " + corporateUserId));
+        }).orElseThrow(() -> new ResourceNotFoundException(
+                "No Corporate User found with corporate_user_id = " + corporateUserId));
         return new ResponseEntity<>(corporateField, HttpStatus.CREATED);
     }
 
     // Update Corporate Field
     @PutMapping("/updateCorpField/{corporateFieldId}")
-    public ResponseEntity<CorporateField> updateCorpField(@PathVariable("corporateFieldId") long corporateFieldId, @RequestBody CorporateField corporateField) {
+    public ResponseEntity<CorporateField> updateCorpField(@PathVariable("corporateFieldId") long corporateFieldId,
+            @RequestBody CorporateField corporateField) {
         CorporateField _api = corporateFieldRepository.findById(corporateFieldId)
-            .orElseThrow(() -> new ResourceNotFoundException("No Corporate Field found with corporate_field_id = " + corporateFieldId));
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "No Corporate Field found with corporate_field_id = " + corporateFieldId));
         _api.setCorporateFieldName(corporateField.getCorporateFieldName());
         return new ResponseEntity<>(corporateFieldRepository.save(_api), HttpStatus.OK);
     }
@@ -132,45 +144,65 @@ public class CorporateFieldController {
     // Delete Corporate Field
     @DeleteMapping("/deleteCorpField/{corporateId}")
     public ResponseEntity<HttpStatus> deleteCorpField(@PathVariable("corporateId") long corporateId) {
-        CorporateUser searchCorporate = corporateUserRepository.findById(corporateId).orElseThrow(() 
-            -> new ResourceNotFoundException("No Corporate found with corporate_id = " + corporateId));
+        CorporateUser searchCorporate = corporateUserRepository.findById(corporateId).orElseThrow(
+                () -> new ResourceNotFoundException("No Corporate found with corporate_id = " + corporateId));
         corporateFieldRepository.deleteAllCorpFieldsByUserId(searchCorporate);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     // // SAMPLE
     // @GetMapping("/getAllCorpFieldByCorpUserId/{corporateId}")
-    // public ResponseEntity<List<CorporateField>> getAllCorpFieldByCorpUserId(@PathVariable(value = "corporateId") Long corporateId) {
-    //     CorporateUser searchCorporate = corporateUserRepository.findById(corporateId).orElseThrow(() 
-    //         -> new ResourceNotFoundException("No Corporate found with corporate_id = " + corporateId));
-    //     if (searchCorporate == null) {
-    //         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    //     }
-    //     List<CorporateField> corporateFields = corporateFieldRepository.findAllCorpFieldByUserId(searchCorporate);
-    //     if (corporateFields.isEmpty()) {
-    //         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-    //     }
-    //     return new ResponseEntity<>(corporateFields, HttpStatus.OK);
+    // public ResponseEntity<List<CorporateField>>
+    // getAllCorpFieldByCorpUserId(@PathVariable(value = "corporateId") Long
+    // corporateId) {
+    // CorporateUser searchCorporate =
+    // corporateUserRepository.findById(corporateId).orElseThrow(()
+    // -> new ResourceNotFoundException("No Corporate found with corporate_id = " +
+    // corporateId));
+    // if (searchCorporate == null) {
+    // return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    // }
+    // List<CorporateField> corporateFields =
+    // corporateFieldRepository.findAllCorpFieldByUserId(searchCorporate);
+    // if (corporateFields.isEmpty()) {
+    // return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    // }
+    // return new ResponseEntity<>(corporateFields, HttpStatus.OK);
     // }
 
-
     @PostMapping("/uploadExcelHeader/{corporateUserId}")
-    public ResponseEntity<ResponseMessage> addFieldMapping(@PathVariable("corporateUserId") long corporateUserId, @RequestParam("file") MultipartFile file) {
+    public ResponseEntity<ResponseMessage> addFieldMapping(@PathVariable("corporateUserId") long corporateUserId,
+            @RequestParam("file") MultipartFile file) {
         String message = "";
+
         try {
+            // Retrieve List of Corporate Fields
+            CorporateUser searchCorporate = corporateUserRepository.findById(corporateUserId).orElseThrow(
+                    () -> new ResourceNotFoundException("No Corporate found with corporate_id = " + corporateUserId));
+            if (searchCorporate == null) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            List<CorporateField> corporateFields = corporateFieldRepository.findAllCorpFieldByUserId(searchCorporate);
+            if (corporateFields.size() != 0) {
+                deleteCorpField(corporateUserId);
+            }
             Workbook workbook = new XSSFWorkbook(file.getInputStream());
             // Reading the first sheet
             Sheet sh = workbook.getSheetAt(0);
             Row header = sh.getRow(0);
             Iterator<Cell> iterHeader = header.iterator();
             // Searching for a user by {corporateUserId}
-            CorporateUser corporateUser = corporateUserRepository.findById(corporateUserId).orElseThrow(() 
-                -> new ResourceNotFoundException("No Corporate User found with corporate_user_id = " + corporateUserId));
+            CorporateUser corporateUser = corporateUserRepository.findById(corporateUserId)
+                    .orElseThrow(() -> new ResourceNotFoundException(
+                            "No Corporate User found with corporate_user_id = " + corporateUserId));
 
-            // // Check if headers exist --> if it already does, return Error 400 Bad Request
+            // // Check if headers exist --> if it already does, return Error 400 Bad
+            // Request
             // if (!(checkIfHeadersExist(iterHeader, corporateUserId))) {
-            //     message += "You have already submitted these corporate field headers. You cannot submit the same ones twice";
-            //     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message));
+            // message += "You have already submitted these corporate field headers. You
+            // cannot submit the same ones twice";
+            // return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new
+            // ResponseMessage(message));
             // }
 
             // Iterate through the columns within a row
@@ -185,28 +217,31 @@ public class CorporateFieldController {
 
                 message += currentCell + " ";
             }
+
         } catch (IOException e) {
             throw new RuntimeException("fail to store csv data: " + e.getMessage());
         }
         return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
     }
 
-    // public static boolean checkIfHeadersExist(Iterator<Cell> iterHeader, long corporateUserId){
-    //     ArrayList<String> itemList = new ArrayList<>();
-    //     while (iterHeader.hasNext()) {
-    //         String currentCell = iterHeader.next().toString();
-    //         itemList.add(currentCell);
-    //     }
-    //     CorporateFieldController newCon = new CorporateFieldController();
-    //     ResponseEntity<List<CorporateField>> newCU = newCon.getAllCorpFieldByCorpUserId(corporateUserId);
-    //     List<CorporateField> cfList = newCU.getBody();
-    //     for (CorporateField cf : cfList) {
-    //         String cfName = cf.getCorporateFieldName();
-    //         if (itemList.contains(cfName) == false) {
-    //             return true;
-    //         }
-    //     }
-    //     return false;
-        
+    // public static boolean checkIfHeadersExist(Iterator<Cell> iterHeader, long
+    // corporateUserId){
+    // ArrayList<String> itemList = new ArrayList<>();
+    // while (iterHeader.hasNext()) {
+    // String currentCell = iterHeader.next().toString();
+    // itemList.add(currentCell);
     // }
-    }
+    // CorporateFieldController newCon = new CorporateFieldController();
+    // ResponseEntity<List<CorporateField>> newCU =
+    // newCon.getAllCorpFieldByCorpUserId(corporateUserId);
+    // List<CorporateField> cfList = newCU.getBody();
+    // for (CorporateField cf : cfList) {
+    // String cfName = cf.getCorporateFieldName();
+    // if (itemList.contains(cfName) == false) {
+    // return true;
+    // }
+    // }
+    // return false;
+
+    // }
+}
