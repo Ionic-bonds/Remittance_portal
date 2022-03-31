88e8ee34fadc093e6f11cc6f3b7b48119fff106c
Yeo Yu Quan <yuquanyeo@gmail.com>
2022-03-31 20:43:52 +0800

    Refactoring

--- src/main/java/com/controller/FieldMappingController.java
+++ src/main/java/com/controller/FieldMappingController.java
@@ -38,8 +38,8 @@ import com.request.SendTransaction;
 import com.request.TransactOutcome;
 import com.request.TransactionAuth;
 import com.response.TransactResponse;
+import com.response.UserMappedFieldResponse;
 
-import org.apache.commons.collections4.IteratorUtils;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.ss.usermodel.Row;
@@ -51,9 +51,11 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -80,6 +82,302 @@ public class FieldMappingController {
     private TransactionRepository transactionRepository;
     @Autowired
     private UserApiRequestRepository userApiRequestRepository;
+
+
+    // Get all Api
+    @GetMapping("/getAllApi")
+    public ResponseEntity<List<Api>> getAllApis() {
+        List<Api> apis = apiRepository.findAll();
+        if (apis.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(apis, HttpStatus.OK);
+    }
+
+    // Get Api by api_id
+    @GetMapping("/getApiById/{apiId}")
+    public ResponseEntity<Api> getApiById(@PathVariable("apiId") long apiId) {
+        Api api = apiRepository.findById(apiId).orElseThrow(() 
+            -> new ResourceNotFoundException("No Api found with api_id = " + apiId));
+        return new ResponseEntity<>(api, HttpStatus.OK);
+    }
+
+    // Add new Api
+    @PostMapping("/addApi")
+    public ResponseEntity<Api> addApi(@RequestBody Api api) {
+        Api _api = apiRepository.save(new Api(api.getApiName(), api.getMinAmount(), api.getMaxAmount()));
+        return new ResponseEntity<>(_api, HttpStatus.CREATED);
+    }
+
+    // Get all Api Fields
+    @GetMapping("/getAllApiField")
+    public ResponseEntity<List<ApiField>> getAllApiField() {
+        List<ApiField> apiFields = apiFieldRepository.findAll();
+        if (apiFields.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(apiFields, HttpStatus.OK);
+    }
+
+    // Get Api Field by api_field_id
+    @GetMapping("/getApiFieldById/{apiFieldId}")
+    public ResponseEntity<ApiField> getApiFieldById(@PathVariable("apiFieldId") long apiFieldId) {
+        ApiField apiField = apiFieldRepository.findById(apiFieldId).orElseThrow(() 
+            -> new ResourceNotFoundException("No Api found with api_field_id = " + apiFieldId));
+        return new ResponseEntity<>(apiField, HttpStatus.OK);
+    }
+
+    // Get all Api Fields by api_id
+    @GetMapping("/getAllApiFieldByApiId/{apiId}")
+    public ResponseEntity<List<ApiField>> getAllApiFieldByApiId(@PathVariable(value = "apiId") Long apiId) {
+        Api searchApi = apiRepository.findById(apiId).orElseThrow(() 
+            -> new ResourceNotFoundException("No Api found with api_id = " + apiId));
+        if (searchApi == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        List<ApiField> apiFields = apiFieldRepository.findAllApiFieldByApiId(searchApi);
+        if (apiFields.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(apiFields, HttpStatus.OK);
+    }
+
+    // Add new Api Field
+    @PostMapping("/addApiField/{apiId}")
+    public ResponseEntity<ApiField> addApiField(@PathVariable(value = "apiId") Long apiId, @RequestBody ApiField apiFieldRequest) {
+        ApiField apiField = apiRepository.findById(apiId).map(api -> { 
+            apiFieldRequest.setApi(api);
+            return apiFieldRepository.save(apiFieldRequest);
+        }).orElseThrow(() -> new ResourceNotFoundException("No Api found with api_id = " + apiId));
+        return new ResponseEntity<>(apiField, HttpStatus.CREATED);
+    }
+
+    // Get all Selected Fields
+    @GetMapping("/getAllSelectedField")
+    public ResponseEntity<List<SelectedField>> getAllSelectedField() {
+        List<SelectedField> apis = selectedFieldRepository.findAll();
+        if (apis.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(apis, HttpStatus.OK);
+    }
+
+    // Get Selected Field by selected_field_id
+    @GetMapping("/getSelectedFieldById/{selectedFieldId}")
+    public ResponseEntity<SelectedField> getSelectedFieldById(@PathVariable("selectedFieldId") long selectedFieldId) {
+        SelectedField selectedField = selectedFieldRepository.findById(selectedFieldId).orElseThrow(() 
+            -> new ResourceNotFoundException("No Api found with api_field_id = " + selectedFieldId));
+        return new ResponseEntity<>(selectedField, HttpStatus.OK);
+    }
+
+    // Get all Selected Field by api_field_id
+    @GetMapping("/getAllSelectedByApiFieldId/{apiFieldId}")
+    public ResponseEntity<List<SelectedField>> getAllSelectedByApiFieldId(@PathVariable(value = "apiFieldId") Long apiFieldId) {
+        ApiField searchApiField = apiFieldRepository.findById(apiFieldId).orElseThrow(() 
+            -> new ResourceNotFoundException("No Api found with api_field_id = " + apiFieldId));
+        if (searchApiField == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+
+        List<SelectedField> selectedFields = selectedFieldRepository.findAllSelectedByApiFieldId(searchApiField);
+        if (selectedFields.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(selectedFields, HttpStatus.OK);
+    }
+    
+    // Add new Selected Field
+    @PostMapping("/addSelectedField/{apiFieldId}")
+    public ResponseEntity<SelectedField> addSelectedField(@PathVariable(value = "apiFieldId") Long apiFieldId, @RequestBody SelectedField selectedFieldRequest) {
+        SelectedField selectedField = apiFieldRepository.findById(apiFieldId).map(apiField -> { 
+            selectedFieldRequest.setApiField(apiField);
+            return selectedFieldRepository.save(selectedFieldRequest);
+        }).orElseThrow(() -> new ResourceNotFoundException("No Api Field found with api_field_id = " + apiFieldId));
+        return new ResponseEntity<>(selectedField, HttpStatus.CREATED);
+    }
+
+
+    // Get all Corporate Users
+    @GetMapping("/getAllCorpUsers")
+    public ResponseEntity<List<CorporateUser>> getAllCorpUsers() {
+        List<CorporateUser> apis = corporateUserRepository.findAll();
+        if (apis.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(apis, HttpStatus.OK);
+    }
+
+    // Get Corporate User by corporate_user_id
+    @GetMapping("/getCorpUserById/{corporateUserId}")
+    public ResponseEntity<CorporateUser> getCorpUserById(@PathVariable("corporateUserId") long corporateUserId) {
+        CorporateUser corporateUser = corporateUserRepository.findById(corporateUserId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "No Corporate User found with corporate_user_id = " + corporateUserId));
+        return new ResponseEntity<>(corporateUser, HttpStatus.OK);
+    }
+
+    // Update Corporate User
+    // @PutMapping("/updateCorpUser/{corporateUserId}")
+    public void updateCorpUser(long corporateUserId, CorporateUser corporateUser) {
+        CorporateUser _api = corporateUserRepository.findById(corporateUserId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "No Corporate User found with corporate_user_id = " + corporateUserId));
+        _api.setEmail(corporateUser.getEmail());
+        _api.setPassword(corporateUser.getPassword());
+        _api.setHeaderRow(corporateUser.getHeaderRow());
+        // return new ResponseEntity<>(corporateUserRepository.save(_api), HttpStatus.OK);
+    }
+
+    // Get all Corporate Fields
+    @GetMapping("/getAllCorpField")
+    public ResponseEntity<List<CorporateField>> getAllCorpField() {
+        List<CorporateField> corporateFields = corporateFieldRepository.findAll();
+        if (corporateFields.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(corporateFields, HttpStatus.OK);
+    }
+
+    // Get Corporate Field by corporate_field_id
+    @GetMapping("/getCorpFieldById/{corporateFieldId}")
+    public ResponseEntity<CorporateField> getCorpFieldById(@PathVariable("corporateFieldId") long corporateFieldId) {
+        CorporateField corporateField = corporateFieldRepository.findById(corporateFieldId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "No Corporate Field found with corporate_field_id = " + corporateFieldId));
+        return new ResponseEntity<>(corporateField, HttpStatus.OK);
+    }
+
+    @GetMapping("/getAllCorpFieldByCorpUserId/{corporateId}")
+    public ResponseEntity<UserMappedFieldResponse> getAllCorpFieldByCorpUserId(
+            @PathVariable(value = "corporateId") Long corporateId) {
+        CorporateUser searchCorporate = corporateUserRepository.findById(corporateId).orElseThrow(
+                () -> new ResourceNotFoundException("No Corporate found with corporate_id = " + corporateId));
+        if (searchCorporate == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        List<CorporateField> corporateFields = corporateFieldRepository.findAllCorpFieldByUserId(searchCorporate);
+        List<ApiField> apiFields = apiFieldRepository.findAll();
+        if (apiFields.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        // User has not uploaded any corporate fields
+        UserMappedFieldResponse userMappedFieldResponse = new UserMappedFieldResponse(corporateFields, apiFields);
+        return new ResponseEntity<>(userMappedFieldResponse, HttpStatus.OK);
+    }
+
+    // Get all Corporate Fields by corporate_user_id
+    @GetMapping("/getAllCorpFieldByCorpUserIdOld/{corporateId}")
+    public ResponseEntity<List<CorporateField>> getAllCorpFieldByCorpUserIdOld(
+            @PathVariable(value = "corporateId") Long corporateId) {
+        CorporateUser searchCorporate = corporateUserRepository.findById(corporateId).orElseThrow(
+                () -> new ResourceNotFoundException("No Corporate found with corporate_id = " + corporateId));
+        if (searchCorporate == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        List<CorporateField> corporateFields = corporateFieldRepository.findAllCorpFieldByUserId(searchCorporate);
+        if (corporateFields.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(corporateFields, HttpStatus.OK);
+    }
+
+    // Add new Corporate Field
+    @PostMapping("/addCorpField/{corporateUserId}")
+    public ResponseEntity<CorporateField> addCorpField(@PathVariable(value = "corporateUserId") long corporateUserId,
+            @RequestBody CorporateField corporateFieldRequest) {
+        CorporateField corporateField = corporateUserRepository.findById(corporateUserId).map(corporateUser -> {
+            corporateFieldRequest.setCorporateUser(corporateUser);
+            return corporateFieldRepository.save(corporateFieldRequest);
+        }).orElseThrow(() -> new ResourceNotFoundException(
+                "No Corporate User found with corporate_user_id = " + corporateUserId));
+        return new ResponseEntity<>(corporateField, HttpStatus.CREATED);
+    }
+
+    // Update Corporate Field
+    @PutMapping("/updateCorpField/{corporateFieldId}")
+    public ResponseEntity<CorporateField> updateCorpField(@PathVariable("corporateFieldId") long corporateFieldId,
+            @RequestBody CorporateField corporateField) {
+        CorporateField _api = corporateFieldRepository.findById(corporateFieldId)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "No Corporate Field found with corporate_field_id = " + corporateFieldId));
+        _api.setCorporateFieldName(corporateField.getCorporateFieldName());
+        return new ResponseEntity<>(corporateFieldRepository.save(_api), HttpStatus.OK);
+    }
+
+    // Delete Corporate Field
+    @DeleteMapping("/deleteCorpField/{corporateId}")
+    public ResponseEntity<HttpStatus> deleteCorpField(@PathVariable("corporateId") long corporateId) {
+        CorporateUser searchCorporate = corporateUserRepository.findById(corporateId).orElseThrow(
+                () -> new ResourceNotFoundException("No Corporate found with corporate_id = " + corporateId));
+        corporateFieldRepository.deleteAllCorpFieldsByUserId(searchCorporate);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/uploadExcelHeader/{corporateUserId}/{headerRow}")
+    public ResponseEntity<Object> addFieldMapping(@PathVariable("corporateUserId") long corporateUserId,
+            @PathVariable("headerRow") int headerRow,
+            @RequestParam("file") MultipartFile file) {
+
+        try {
+            // Retrieve List of Corporate Fields
+            CorporateUser searchCorporate = corporateUserRepository.findById(corporateUserId).orElseThrow(
+                    () -> new ResourceNotFoundException("No Corporate found with corporate_id = " + corporateUserId));
+            if (searchCorporate == null) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            List<CorporateField> corporateFields = corporateFieldRepository.findAllCorpFieldByUserId(searchCorporate);
+            if (corporateFields.size() != 0) {
+                deleteCorpField(corporateUserId);
+            }
+            Workbook workbook = new XSSFWorkbook(file.getInputStream());
+            // Reading the first sheet
+            Sheet sh = workbook.getSheetAt(0);
+            // If headers in row 1 of Excel sheet, parameter is 0; if headers in row 2, parameter is 1
+            headerRow -= 1;
+            Row header = sh.getRow(headerRow);
+            Iterator<Cell> iterHeader = header.iterator();
+            // Searching for a user by {corporateUserId}
+            CorporateUser corporateUser = corporateUserRepository.findById(corporateUserId)
+                    .orElseThrow(() -> new ResourceNotFoundException(
+                            "No Corporate User found with corporate_user_id = " + corporateUserId));
+            corporateUser.setHeaderRow(headerRow);
+            updateCorpUser(corporateUserId, corporateUser);
+
+            // // Check if headers exist --> if it already does, return Error 400 Bad
+            // Request
+            // if (!(checkIfHeadersExist(iterHeader, corporateUserId))) {
+            // message += "You have already submitted these corporate field headers. You
+            // cannot submit the same ones twice";
+            // return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new
+            // ResponseMessage(message));
+            // }
+            
+            int colCounter = 1;
+            // Iterate through the columns within a row
+            while (iterHeader.hasNext()) {
+                String currentCell = iterHeader.next().toString();
+                // Declaring empty CorporateUser
+                CorporateField newCorpField = new CorporateField();
+                // Setting new CorporateUser
+                String newCorporateFieldName = currentCell + "_" + String.valueOf(colCounter++);
+                newCorpField.setCorporateFieldName(newCorporateFieldName);
+                newCorpField.setCorporateUser(corporateUser);
+                corporateFieldRepository.save(newCorpField);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException("fail to store csv data: " + e.getMessage());
+        }
+        CorporateUser searchCorporate = corporateUserRepository.findById(corporateUserId).orElseThrow(
+                () -> new ResourceNotFoundException("No Corporate found with corporate_id = " + corporateUserId));
+        List<CorporateField> corpFields = corporateFieldRepository.findAllCorpFieldByUserId(searchCorporate);
+        return new ResponseEntity<>(corpFields, HttpStatus.OK);
+    }
+
+
+
+
+
 
     public Api determineApi(List<Api> apiList, double amount) {
         Api searchApi = null;
@@ -393,6 +691,7 @@ public class FieldMappingController {
             else if (commonApi instanceof PaymentGo) {
                 apiName = "paymentgo";
             }
+            // String transactionToken = authSandbox();
             SendTransaction credentials = new SendTransaction(transactionToken, apiName, commonApi);
             HttpEntity<SendTransaction> requestEntity = new HttpEntity<>(credentials);
             ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, requestEntity, String.class);
