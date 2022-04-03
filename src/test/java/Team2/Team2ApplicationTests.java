9e16e8462c2e48f061ab7a13483a891e81e4a2c1
Yeo Yu Quan <yuquanyeo@gmail.com>
2022-04-04 00:01:48 +0800

    AWS Deployment

--- src/test/java/Team2/Team2ApplicationTests.java
+++ src/test/java/Team2/Team2ApplicationTests.java
@@ -3,7 +3,7 @@ package Team2;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Team2ApplicationTests.class)
 class Team2ApplicationTests {
 
 	@Test
