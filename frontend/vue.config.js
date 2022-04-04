f4829c79b7090a4eb39bd2982ea8c383232ce3ed
Gan Shao Hong <shaohong.g97@gmail.com>
2022-04-04 23:24:58 +0800

    add test files for upload

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-//   devServer: {
-//     proxy: 'https://localhost:8080'
-// } 
+  devServer: {
+    proxy: 'https://localhost:8080'
+} 
 })
